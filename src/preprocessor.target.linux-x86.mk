6d455ce3bce1e1e07eaaf02ff74e645488d28c76
Android Chromium Automerger <chromium-automerger@android>
2013-11-01 15:38:30 +0000

    Update makefiles after merge of Chromium at 232403
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I88ab7170ec8a80ec9df4bf17e5234146518ba893

--- src/preprocessor.target.linux-x86.mk
+++ src/preprocessor.target.linux-x86.mk
@@ -62,11 +62,11 @@ MY_CFLAGS_Debug := \
 	-Wno-extra \
 	-Wno-ignored-qualifiers \
 	-Wno-type-limits \
+	-fno-stack-protector \
 	-Wno-address \
 	-Wno-format-security \
 	-Wno-return-type \
 	-Wno-sequence-point \
-	-fno-stack-protector \
 	-Os \
 	-g \
 	-fomit-frame-pointer \
@@ -146,11 +146,11 @@ MY_CFLAGS_Release := \
 	-Wno-extra \
 	-Wno-ignored-qualifiers \
 	-Wno-type-limits \
+	-fno-stack-protector \
 	-Wno-address \
 	-Wno-format-security \
 	-Wno-return-type \
 	-Wno-sequence-point \
-	-fno-stack-protector \
 	-Os \
 	-fno-ident \
 	-fdata-sections \
